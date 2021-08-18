c4a2d4699e0834744c6a9dc6c92b6a8bfce1d2f2
Elise <elseytd@gmail.com>
2021-08-18 14:44:35 -0700

    fixed "x" display issue when joining forge servers

--- src/main/java/com/lootbeams/LootBeams.java
+++ src/main/java/com/lootbeams/LootBeams.java
@@ -1,9 +1,12 @@
 package com.lootbeams;
 
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,6 +17,7 @@ public class LootBeams {
 	public static final Logger LOGGER = LogManager.getLogger();
 
 	public LootBeams() {
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Configuration.CLIENT_CONFIG);
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(ClientSetup::init);
 	}
