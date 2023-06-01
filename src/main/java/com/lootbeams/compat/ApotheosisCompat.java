bb4832b66373e04a9540cbdea63a98ff62803c2c
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2023-06-01 16:55:48 -0500

    Fix Apotheosis Crash

--- src/main/java/com/lootbeams/compat/ApotheosisCompat.java
+++ src/main/java/com/lootbeams/compat/ApotheosisCompat.java
@@ -2,15 +2,15 @@ package com.lootbeams.compat;
 
 import net.minecraft.world.item.ItemStack;
 import shadows.apotheosis.adventure.affix.AffixHelper;
-import shadows.apotheosis.adventure.loot.LootRarity;
 
 public class ApotheosisCompat {
 
     public static String getRarityName(ItemStack stack){
-        if(!isApotheosisItem(stack)) return "common";
-        LootRarity rarity = AffixHelper.getRarity(stack);
-        if(rarity == null) return "common";
-        return rarity.id().toLowerCase();
+        if (!isApotheosisItem(stack) || AffixHelper.getRarity(stack) == null) {
+            return "null";
+        }
+
+        return AffixHelper.getRarity(stack).id().toLowerCase();
     }
 
     public static boolean isApotheosisItem(ItemStack stack){
